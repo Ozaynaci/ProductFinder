9ae54aa9b2518438f5029c3dd67ac21e851cf36d
Ozaynaci <hsnzync_@hotmail.com>
2017-04-15 13:45:50 +0200

    init

---
+++ main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Find a smartphone</title>
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="node_modules/dist/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="node_modules/dist/css/main.css" rel="stylesheet" type="text/css">
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="node_modules/dist/js/plugins/purify.min.js" type="text/javascript"></script>
+    <script src="node_modules/dist/js/fileinput.min.js"></script>
+
+  </head>
+  <body class="main">
+    <h1>Find an used smartphone</h1>
+    <div id="map"></div>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
